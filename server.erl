6b5178f3863cb657fa91e6810d8082cc868b1533
Christoffer Medin <meddan@dhcp-181175.eduroam.chalmers.se>
2014-02-20 11:12:45 +0100

    A lot of fixes in server.

--- server.erl
+++ server.erl
@@ -9,10 +9,10 @@ loop(St, _Msg) ->
 % User connecting to the server
 %
 request(State, {connect, {UserID,UserPID}}) ->
-	UserConnected = list:member(UserID, State#server_st.users),
+	UserConnected = lists:member({UserID,UserPID}, State#server_st.users),
 	if
 		not UserConnected ->
-			{ok_connected, State#server_st{users = list:append([{UserID,UserPID}], State#server_st.users)}};
+			{ok_connected, State#server_st{users = lists:append([{UserID,UserPID}], State#server_st.users)}};
 		true ->
 			{{error, user_already_connected}, State}
 	end;
@@ -22,12 +22,12 @@ request(State, {connect, {UserID,UserPID}}) ->
 % User disconnecting from the server
 %
 request(State, {disconnect, {UserID,UserPID}}) ->
-	UserConnected = list:member(UserID, State#server_st.users),
+	UserConnected = lists:member({UserID,UserPID}, State#server_st.users),
 	if
 		not UserConnected ->
 			{{error, user_not_connected}, State};
 		true ->
-			{ok, State#server_st{users = list:delete([UserID], State#server_st.users)}}
+			{ok, State#server_st{users = lists:delete([{UserID,UserPID}], State#server_st.users)}}
 	end;
 
 %
@@ -44,9 +44,9 @@ request(State, {join, {UserID,UserPID}, ChannelName}) ->
 				%User is not a member of the channel
 				false ->
 				%Add the user to the channel
-				ChangedChannel = Tuple#channel{users = list:append(Tuple#channel.users, [UserID])},
+				ChangedChannel = Tuple#channel{users = lists:append(Tuple#channel.users, [UserID])},
 				%Remove the old reference of the channel and add a new one.
-				{ok, State#server_st{channels = list:append(list:delete(Tuple, State#server_st.channels), [ChangedChannel])}};
+				{ok, State#server_st{channels = lists:append(lists:delete(Tuple, State#server_st.channels), [ChangedChannel])}};
 
 				% If the user is already a member of the channel.
 				true -> {{error,user_already_joined}, State}
@@ -54,14 +54,15 @@ request(State, {join, {UserID,UserPID}, ChannelName}) ->
 		%Channel doesn't exist
 		false ->
 			%Create new channel and add the user to it.
-			{ok, State#server_st{channels = list:append(State#server_st.channels, NewChannel = #channel{name = ChannelName, users = [UserID]})}}
+			{ok, State#server_st{channels = lists:append(State#server_st.channels, NewChannel = #channel{name = ChannelName, users = [UserID]})}}
 	end;
 %
 % User sending a message to a channel
 %
 request(State, {message, {UserID,UserPID}, ChannelName, Token}) ->
 	{value, Channel} = lists:keysearch(ChannelName,1,State#server_st.channels),
-	lists:keymap( ! {message, UserID, ChannelName, Token}, 2 , Channel.#channel.users),
+	ListOfUsers = Channel#channel.users,
+	[Pid ! {message, UserID, ChannelName, Token} || Pid <- ListOfUsers],
 	{ok, State}.
 
 
