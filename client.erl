27dac858333a1507975d24a1e190e3d1d8840f00
Pontus Doverstav <doverstav@hotmail.com>
2014-02-20 09:26:15 +0100

    Added already implemented code
    This is a large part of client.erl and the definition of the client
    state in defs.hrl

--- client.erl
+++ client.erl
@@ -7,19 +7,53 @@
 %%%% Connect
 %%%%%%%%%%%%%%%
 loop(St, {connect, _Server}) ->
-    {ok, St} ;
+    Ref = make_ref(),
+    case catch (list_to_atom(_Server) ! {request, self(), Ref, {connect, St#cl_st.nick}}) of
+        {'EXIT', Reason} -> % There is no server like this
+            {{error, server_not_reached, "Could not connect to server!"}, St};
+        _Else ->
+            receive
+                {exit, Ref, Reason} -> % Server crashed
+                    {'EXIT', "Server crashed"};
+                {result, Ref, ok_connected} -> % Connected
+                    {ok, St#cl_st{server = _Server}};
+                {result, Ref, {error, user_already_connected}} -> % Could not connect
+                    {{error, user_already_connected, "User already connected"}, St}
+            end
+    end;
 
 %%%%%%%%%%%%%%%
 %%%% Disconnect
 %%%%%%%%%%%%%%%
 loop(St, disconnect) ->
-     {ok, St} ;
+    Ref = make_ref(),
+    case catch list_to_atom(St#cl_st.server) ! {request, self(), Ref, {disconnect, St#cl_st.nick}} of
+        {'EXIT', Reason} -> % Server could not be reached
+            {{error, server_not_reached, "Could not reach server"}, St};
+        _Else ->
+            receive
+                {result, Ref, ok} -> % Disconnected
+                    {ok, St};
+                {result, Ref, {error, user_not_connected}} -> % User was never connected
+                    {{error, user_not_connected, "User not connected"}, St};
+                {result, Ref, {error, leave_channels_first}} -> % User hasn't left the channels first
+                    {{error, leave_channels_first, "Leave channels before disconnecting"}, St}
+            end
+    end;
+
 
 %%%%%%%%%%%%%%
 %%% Join
 %%%%%%%%%%%%%%
 loop(St,{join,_Channel}) ->
-    {ok, St} ;
+    Ref = make_ref(),
+    list_to_atom(St#cl_st.server) ! {request, self(), Ref, {join, St#cl_st.nick, _Channel}},
+    receive
+        {result, Ref, ok} -> % Join went ok
+            {ok, St};
+        {result, Ref, {error, user_already_joined}} -> % User has already joined thic channel before
+            {{error, user_already_joined, "You've already joined this channel!"}, St}
+    end;
 
 %%%%%%%%%%%%%%%
 %%%% Leave
@@ -38,13 +72,13 @@ loop(St, {msg_from_GUI, _Channel, _Msg}) ->
 %%% WhoIam
 %%%%%%%%%%%%%%
 loop(St, whoiam) ->
-    {"User01", St} ;
+    {St#cl_st.nick, St} ;
 
 %%%%%%%%%%
 %%% Nick
 %%%%%%%%%%
 loop(St,{nick,_Nick}) ->
-    {ok, St} ;
+    {ok, St#cl_st{nick = _Nick}} ;
 
 %%%%%%%%%%%%%
 %%% Debug
@@ -64,10 +98,12 @@ loop(St = #cl_st { gui = GUIName }, _MsgFromClient) ->
 % This function will take a message from the client and
 % decomposed in the parts needed to tell the GUI to display
 % it in the right chat room.
+decompose_msg({msg_from_GUI, Chatroom, String}) -> 
+    {msg_from_GUI, Chatroom, String};
+
 decompose_msg(_MsgFromClient) ->
-
-decompose_msg({msg_from_GUI, Chatroom, String}) -> {msg_from_GUI, Chatroom, String}.
-
+    {"", "", ""}.
 
 initial_state(Nick, GUIName) ->
-    #cl_st { gui = GUIName }.
+    #cl_st { gui = GUIName, nick = Nick }.
+
